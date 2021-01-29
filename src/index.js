b71e04745a1551e214e828707cc253debd4ded8d
owaisfarid <mowaisfarid@gmail.com>
2021-01-29 12:03:42 +0500

    class code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Room(){
+const [value,setvalue]=useState(true)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function abc(){
+//   setvalue(false)
+
+// }
+
+function abc(){
+  setvalue((preVal)=>(!preVal))
+
+}
+
+return(
+  <div>
+    <div> the room is {(value===true)? "Bright":"Dark"} </div>
+    <button onClick={abc}>Change</button>
+  </div>
+)
+}
+
+ReactDOM.render(<Room/>),document.getElementById('root')
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
